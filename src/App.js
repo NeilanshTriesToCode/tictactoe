34cf75602a5f508acb1c2f35a1aeeba57c5b8715
NeilanshTriesToCode <neilanshjr@gmail.com>
2021-05-26 11:48:03 +0530

    transform state to game history

--- src/App.js
+++ src/App.js
@@ -8,51 +8,72 @@ const App = () => {
      1. the original argument passed to it
      2. a function used to update the initial state
   */
-    // initial state of board: all elements (9 square-grids) are null. They are being represented as an array.
+    // initial state of game: history is an array of objects.
+    /* each object contains two elements: 
+       1. board (array of 9 elements) to keep track of values of square-grids that are clicked on
+        2. next element (either a X or a O) 
+    */
     // the state of an element/component can be changed using events (for eg. the onClick method in Square.js)
-    const [board, setBoard] = useState(Array(9).fill(null)); // initial state
+    const [history, setHistory] = useState([
+        { board: Array(9).fill(null), isXNext: true },
+    ]); // initial state
 
-    // state to keep track of the next player (in order to determine whether to write 'X' or 'O' on the square-grid)
-    const [isXNext, setIsXNext] = useState(false); // default is false, i.e., always start with an 'O'
+    const [currentMove, setCurrentMove] = useState(0); // to keep track of steps/moves in the game
+
+    const current = history[currentMove];
 
     // function used to determine the winner
-    const winner = calculateWinner(board); // returns winner (X or O) if there is one, else null
+    const winner = calculateWinner(current.board); // returns winner (X or O) if there is one, else null
 
     // message to print
     const message = winner
         ? `Winner is ${winner}`
-        : `Next player: ${isXNext ? 'X' : 'O'}`;
+        : `Next player: ${current.isXNext ? 'X' : 'O'}`;
 
     // function to handle when a square-grid is clicked on
     const handleSquareClick = position => {
         // check if the clicked square-grid has been clicked on before to prevent overwriting its value
         // also, if we have a winner, don't let the user play anymore
-        if (board[position] || winner) {
+        if (current.board[position] || winner) {
             return;
         }
 
         // assign a 'X' or 'O' to the square-grid which is clicked on
-        setBoard(prevState => {
+        // the setHistory() function receives the history array which it further updates
+        setHistory(prevState => {
+            // the history (sent to this function) array is being referred to as prevState
             // prevState is the array containing current values of all 9 square-grids at their respective indices
             // map the array elements: assign a value to the grid which is clicked on
-            return prevState.map((square, index) => {
+
+            const last = prevState[prevState.length - 1]; // getting the last element of the prevState (history) array
+
+            const newBoard = last.board.map((square, index) => {
                 if (index === position) {
                     // if the index of the array matches the position of the grid clicked on, return 'X'
-                    return isXNext ? 'X' : 'O'; // used to change the value of grid clicked on. Value written is 'X' or 'O' based on what isXNext returns
+                    return last.isXNext ? 'X' : 'O'; // used to change the value of grid clicked on. Value written is 'X' or 'O' based on what isXNext returns
                 }
                 return square; // else return square (do nothing)
             });
+
+            return prevState.concat({
+                board: newBoard,
+                isXNext: !last.isXNext,
+            }); // concatenating to history array
         });
 
-        // switch value of isXNext
-        setIsXNext(prevState => !prevState);
+        // update current move
+        // the setCurrentMove() function receives the currentMove as an argument
+        setCurrentMove(prevState => prevState + 1); // updating state of currentMove
     };
 
     return (
         <div className="app">
             <h1>TIC TAC TOE</h1>
             <h2>{message}</h2>
-            <Board board={board} handleSquareClick={handleSquareClick} />
+            <Board
+                board={current.board}
+                handleSquareClick={handleSquareClick}
+            />
         </div>
     );
 };
